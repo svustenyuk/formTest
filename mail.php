27ec383569eb5fb723b12b69dcf40cbaf2f6628e
svustenyuk <svustenuk@gmail.com>
2017-05-20 22:44:54 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -8,7 +8,7 @@ $priority = $_POST['priority'];
 $type = $_POST['type'];
 $message = $_POST['message'];
 $formcontent=" From: $name \n Phone: $phone \n Call Back: $call \n Website: $website \n Priority: $priority \n Type: $type \n Message: $message";
-$recipient = "svustenuk@gmail.com";
+$recipient = "svustenuk@ukr.net";
 $subject = "svustenuk@mail.ru";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
